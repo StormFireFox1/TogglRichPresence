8bf819975bfe6d1ddc894ac8df3fb1ce37065c4e
Matei-Alexandru Gardus <matei@gard.us>
2020-03-24 21:07:35 -0700

    Add command-line package

---
+++ cmd/togglRichPresence.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"github.com/StormFireFox1/TogglRichPresence"
+	"log"
+	"os"
+	"os/signal"
+	"sort"
+	"syscall"
+	"time"
+
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	flags := []cli.Flag{
+		&cli.StringFlag{
+			Name:     "togglApiKey",
+			Value:    "",
+			Usage:    "API `KEY` for Toggl account",
+			EnvVars:  []string{"TOGGL_API_KEY"},
+			Required: true,
+		},
+		&cli.StringFlag{
+			Name:     "discordAppId",
+			Value:    "",
+			Usage:    "App `ID` for personal Discord Rich Presence app",
+			EnvVars:  []string{"DISCORD_APP_ID"},
+			Required: true,
+		},
+		&cli.StringFlag{
+			Name:    "config, c",
+			Value:   "~/.config/TogglRichPresence/config.json",
+			Usage:   "Load configuration from `FILE`",
+			EnvVars: []string{"CONFIG_PATH"},
+		},
+	}
+
+	app := &cli.App{
+		Flags:  flags,
+		Commands: []*cli.Command{
+			{
+				Name:    "sync",
+				Aliases: []string{"s"},
+				Usage:   "Interactively syncs current time entry in Toggl to Discord Rich Presence.",
+				Action: func(c *cli.Context) error {
+					sigChannel := make(chan os.Signal)
+					signal.Notify(sigChannel, os.Interrupt, syscall.SIGTERM)
+					go func() {
+						<-sigChannel
+						fmt.Println("Interrupt received! Stopping refresh...")
+						os.Exit(1)
+					}()
+
+					discordWrapper := TogglRichPresence.InitializeDiscordWrapper(c.String("discordAppId"))
+					togglWrapper := TogglRichPresence.InitializeTogglWrapper(c.String("togglApiKey"))
+
+					log.Print("Syncing Toggl and Discord Rich Presence!")
+					for {
+						discordWrapper.RefreshRichPresenceToggl(togglWrapper)
+						time.Sleep(60 * time.Second)
+					}
+				},
+			},
+			{
+				Name:    "set",
+				Aliases: []string{"a"},
+				Usage:   "Stops current timer, sets a new Toggl timer to the provided `DESCRIPTION`, `PROJECT`" +
+						 "and `DETAILS`, and also sets the current Rich Presence activity.",
+				Action: func(c *cli.Context) error {
+					log.Fatal("Not yet implemented!")
+					return nil
+				},
+			},
+		},
+	}
+
+	sort.Sort(cli.FlagsByName(app.Flags))
+	sort.Sort(cli.CommandsByName(app.Commands))
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/ananagame/rich-go v0.0.0-20200319172754-527649f3d36d // indirect
 	github.com/buger/jsonparser v0.0.0-20200322175846-f7e751efca13
 	github.com/hugolgst/rich-go v0.0.0-20200319172754-527649f3d36d
+	github.com/urfave/cli/v2 v2.2.0
 	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
 )
